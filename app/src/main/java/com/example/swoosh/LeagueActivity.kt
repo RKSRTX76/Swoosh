434b87fb27c28b5aaa50b53f7b2531ee4dbffa0d
Rajat Kumar Sahoo <sahoo21rajat@gmail.com>
2023-06-09 17:57:25 +0530

    LeagueActivity

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -1,12 +1,21 @@
 package com.example.swoosh
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class LeagueActivity : BasicActivity() {  // inherit property of BasicActivity class
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
-
+        val next:Button=findViewById(R.id.Next)
+        next.setOnClickListener {
+            val skillactivity= Intent(this,SkillActivity::class.java)
+            startActivity(skillactivity)
+        }
     }
 }
