b6b2e71e1cd233b89a9d072a6914f5235faa8cea
Rajat Kumar Sahoo <sahoo21rajat@gmail.com>
2023-06-13 23:15:49 +0530

    FinalActivity

--- app/src/main/java/com/example/swoosh/Controller/FinishActivity.kt
+++ app/src/main/java/com/example/swoosh/Controller/FinishActivity.kt
@@ -2,6 +2,7 @@ package com.example.swoosh.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.swoosh.R
 import com.example.swoosh.Utilities.EXTRA_LEAGUE
 import com.example.swoosh.Utilities.EXTRA_SKILL
@@ -16,6 +17,7 @@ class FinishActivity : AppCompatActivity() {
         leagues=intent.getStringExtra(EXTRA_LEAGUE).toString()
         skills=intent.getStringExtra(EXTRA_SKILL).toString()
 
-        searchLeagueText.text="Looking for $leagues $skills league near you "
+        val search_League_Text:TextView=findViewById(R.id.searchLeagueText)
+        search_League_Text.text="Looking for $leagues $skills league near you "
     }
 }
